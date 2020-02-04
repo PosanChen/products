655fc050db5c6b18c321ed932023d91e31eeab55
Posan Chene <eric_010223@hotmail.com>
2020-02-04 13:22:34 +0800

    csv version

--- product.py
+++ product.py
@@ -14,8 +14,12 @@ for product in products:
 	print(product)
 	print(product[0], '的價格是', p[1])
 
-products[0][0]
-
-
 #四三行變一行，p = [name, price]
 #三行變一行，products.append([name, price])
+
+with open('products.csv', 'w') as f:
+#打開文件檔寫入 當作"f""
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+#把商品名＋逗點＋價格＋換行 寫入f資料夾
+#寫入csv檔 逗點可以分格
